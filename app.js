aef89f9a4182fd48331c24b6826a6780c04d6390
tinegachris-o <143676466+tinegachris-o@users.noreply.github.com>
2024-11-08 16:25:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ app.use('/api/contacts',  require('./routes/contactRoutes'))
 
 app.use('/api/users',     require('./routes/userRoutes'))
 ////app.use(asyncHandler)
-//iIc21TvSraWeRDd1
-//mongodb+srv://tinegachris797:iIc21TvSraWeRDd1@cluster0.yp6oi.mongodb.net/
+//
 //database 
 //
 connectDB()
